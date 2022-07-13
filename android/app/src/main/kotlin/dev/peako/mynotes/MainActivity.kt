e8b3cd91bf4f7f64dd427d00a835022dfaf38d0e
peakodev <peakodev@gmail.com>
2022-07-13 13:45:31 +0300

    first commit with last work

---
+++ android/app/src/main/kotlin/dev/peako/mynotes/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.peako.mynotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:mynotes/firebase_options.dart';
+import 'package:mynotes/views/login_view.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Home')),
+        body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                final user = FirebaseAuth.instance.currentUser;
+                if (user?.isAnonymous ?? false) {
+                  return const Text('You are not auth');
+                } else {
+                  if (user?.emailVerified ?? false) {
+                    return const Text('You are verified user');
+                  } else {
+                    return const Text('You need to verify user');
+                  }
+                }
+              default:
+                return const Text('Loading...');
+            }
+          },
+        ));
+  }
+}
